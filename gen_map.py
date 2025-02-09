9fbcf0b879acdd904e2c38075829eba6ef2e5e47
Sơn Tùng Kiều <kieusontung8@gmail.com>
2025-02-09 17:07:10 +0700

    add auto test

--- gen_map.py
+++ gen_map.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-# Set the seed for reproducibility
-np.random.seed(42)
-
 # Define the size of the matrix
-def gen_map(NUM_OF_NODES = 10):
-    n  = NUM_OF_NODES  # You can change this value to any desired size
+def gen_map(NUM_OF_NODES = 10,seed = 42):
+    # Set the seed for reproducibility
+    np.random.seed(seed)
+
 # Generate an n x n matrix with random values in the range [0.5, 100]
-    matrix = np.random.uniform(0.5, 100, (n, n))
+    matrix = np.random.uniform(0.5, 100, (NUM_OF_NODES, NUM_OF_NODES))
 
     # Print the generated matrix
     print(matrix)
 
     # Save the matrix to a CSV file
     np.savetxt('distance.csv', matrix, delimiter=',')
+
 gen_map()
--- gen_vehicle.py
+++ gen_vehicle.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-# Set the seed for reproducibility
-np.random.seed(42)
-metric = [9.7, 24.2, 26.7, 32, 38.2, 54]
-def gen_list_vehicle(NUM_OF_VEHICLES):
+def gen_list_vehicle(NUM_OF_VEHICLES, seed = 42):
+    # Given value
+    metric = [9.7, 24.2, 26.7, 32, 38.2, 54]
+    # Set the seed for reproducibility
+    np.random.seed(seed)
     # Generate a list of n random numbers in the range [0, 5]
-    # xe_s =
-    xe_s = [metric[u] for u in [0]*2+[1]*1+[2]*0+[3]*0+[4]*0+[5]*0] if NUM_OF_VEHICLES ==3 else [metric[u] for u in [0]*3+[1]*2+[2]*2+[3]*1+[4]*1+[5]*1] if NUM_OF_VEHICLES == 10 else  [metric[u] for u in [0]*15+[1]*10+[2]*10+[3]*10+[4]*5+[5]*5]
-
-    # Print the generated list
-    print(xe_s)
+    xe_s = [metric[u] for u in [0]*2+[1]*1+[2]*0+[3]*0+[4]*0+[5]*0][:NUM_OF_VEHICLES] if NUM_OF_VEHICLES <=3 \
+    else [metric[u] for u in [0]*3+[1]*2+[2]*2+[3]*1+[4]*1+[5]*1][:NUM_OF_VEHICLES] if NUM_OF_VEHICLES <= 10 \
+    else [metric[u] for u in [0]*15+[1]*10+[2]*10+[3]*10+[4]*5+[5]*5][:NUM_OF_VEHICLES]
 
     # Save the list to a CSV file
     np.savetxt('vehicle.csv', xe_s, delimiter=',', fmt='%.1f')
-gen_list_vehicle(3)
+
+gen_list_vehicle(5)
