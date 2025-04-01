dbae8669baa3f66b584f323995f7d9593d72fc7b
Kieu Son Tung <kieusontung8@gmail.com>
2025-04-01 14:55:15 +0700

    .

--- post_process.py
+++ post_process.py
@@ -4,7 +4,6 @@ import pandas as pd
 from utilities.split_data import read_mapping
 def read_output(filename):
     mapped_requests, mapping, inverse_mapping,node_id_to_request = read_mapping()
-    # print(inverse_mapping)
     df = pd.read_csv("data/destinations.csv")
     try:
         with open(filename, "r", encoding="utf-8") as file:
@@ -68,78 +67,7 @@ def read_output(filename):
 
         json_data[day] = json_day
 
-
-    print(json_data)
     return json_data
-
-
-def read_output_old(filename):
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            output = file.read()
-    except FileNotFoundError:
-        print(f"Lỗi: Không tìm thấy file {filename}!")
-        return None
-    output = output.split("\n---")  # tách các ngày
-    days = []
-    for day in output[1:]:
-        # day = day[2]# tách các xe
-        day = day.split("\n")[11:]
-        day = [s for s in day if s.strip()]  # loại bỏ các dòng trống
-        print(day)
-        exit()
-        vehicles = {}
-        for vehicle in day[1:]:
-            pre_name, vehicle = vehicle.split(
-                ":", maxsplit=1
-            )  # bỏ route for vehicle x:
-            vehicle_id = int(pre_name.split(" ")[-1])  # lấy tên xe
-            vehicle = vehicle.split("->")  # tách các node
-            distance_of_route = -1
-            list_of_route = []
-            for info in vehicle:
-                # chuẩn hoá info
-                if isinstance(info.split("\n"), list) and len(info.split("\n")) > 1:
-                    info = info.split("\n")
-                    if len(info[0]) < 1:
-                        info = info[1]
-                    else:
-                        if "D" in info[1]:
-                            distance_of_route = int(info[1].split(" ")[-1])
-                        info = info[0]
-                # tách thông tin
-                # print(f"info: {info}")
-                info = info.split(" ")
-                node = int(info[2])
-                arrival_time = int(info[5][:-1])
-                capacity = int(info[7][:-1])
-                delivered = int(info[9][:-1])
-                list_of_route.append(
-                    {
-                        "node": node,
-                        "arrival_time": arrival_time,
-                        "capacity": capacity,
-                        "delivered": delivered,
-                    }
-                )
-
-                # print(f"node: {node}")
-                # print(f"arrival_time: {arrival_time}")
-                # print(f"capacity: {capacity}")
-                # print(f"delivered: {delivered}")
-
-            vehicle = {
-                "distance_of_route": distance_of_route,
-                "list_of_route": list_of_route,
-            }
-            if distance_of_route > 0:
-                vehicles[vehicle_id] = vehicle
-            # print(max(day, key = lambda x: len(x)))
-        day = {"vehicles": vehicles}
-        days.append(day)
-        # exit()
-    return days
-
 
 def read_and_save_json_output(
     filename=r"data\stdout_output_2025-02-19_00-00-00.txt",
@@ -161,6 +89,7 @@ def read_and_save_json_output(
             json.dump(output, jsonfile, indent=4)
     return output
 
+# def recalculate_accumulated
 
 if __name__ == "__main__":
     # filename = r"data\stdout_output_2025-02-19_00-00-00.txt"
